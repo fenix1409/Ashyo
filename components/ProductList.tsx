6432a1409e6d6135322a12377421157405b01380
fenix1409 <b2550486@gmail.com>
2024-12-06 23:40:50 +0500

    addition

--- components/ProductList.tsx
+++ components/ProductList.tsx
@@ -12,7 +12,7 @@ const ProductList = () => {
             <ul className="flex items-center gap-[30px] overflow-x-auto pl-10">
                 {products.map(item => (
                     <li key={item.id} className="w-[273px] h-[422px]">
-                        <Image src={'/images/earphone.png'} alt="image" className="bg-cover h-[202px] bg-[#EBEFF3] rounded-lg mb-[16px]" width={202} height={202} />
+                        <Image style={{ width: "280px" , height: "273px"}} src={item.images[0]} alt="image" className="h-[280px] !max-w-[273px] bg-[#EBEFF3] rounded-lg mb-[16px] object-contain" width={273} height={280} />
                         <h2 className="text-[14px] leading-[19px] text-[#545D6A]">{item.name}</h2>
                         <strong className="text-[20px] leading-[26px] font-bold text-[#0A1729]">{item.price} $</strong>
                     </li>
